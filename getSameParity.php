422151d6585f66a0f1987447a3c0da26c2fd5a4d
Max <maxisavinov147@gmail.com>
2018-05-10 22:26:09 +0300

    test1

---
+++ getSameParity.php
@@ -0,0 +1,10 @@
+<?php
+
+function getSameParity($array){
+
+if(empty($array)) {
+    return 'empty array';
+}
+}
+
+print_r(getSameParity([])); // => []
--- test.php
+++ test.php
@@ -1,3 +1,12 @@
 <?php
 
-phpinfo();
+function F($n1)
+{
+if ($n1 > 0)   
+  {     
+    F($n1 - 4);     
+    echo ("$n1");     
+    F($n1 / 3);   
+  } 
+}
+print_r(F(9));
